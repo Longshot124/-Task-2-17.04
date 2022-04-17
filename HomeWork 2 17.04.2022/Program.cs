f1602b1b836f0945d96c8f25ed19df8905a78566
kamra <kamra@LAPTOP-M49P7DNF>
2022-04-17 17:46:08 +0400

    Task 2 finished

---
+++ HomeWork 2 17.04.2022/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HomeWork_2_17._04._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eded elave edin");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bolunen ededi daxil edin");
+            int dividedNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = firstNumber % dividedNumber;
+            int total = 0;
+            int thirdNumber = dividedNumber / 2;
+
+
+
+            if (secondNumber <= thirdNumber)
+            {
+                total = firstNumber - secondNumber;
+                Console.WriteLine("Verilmis edede yaxin bolunen reqem" +" "+ total);
+            }
+            else if (secondNumber > thirdNumber)
+            {
+                total = firstNumber + dividedNumber - secondNumber;
+                Console.WriteLine("verilmis edede yaxin boluen reqem" +" "+ total);
+
+            }
+            else { Console.WriteLine("Eded bolunur"); }
+            
+            Console.ReadLine();
+            
+        }
+    }
+}
